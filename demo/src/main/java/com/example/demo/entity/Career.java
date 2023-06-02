5bf2ebf85482fce53abfc87cef04c021c02764f6
kanado5385-k <81556800+kanado5385-k@users.noreply.github.com>
2023-06-02 16:09:09 +0900

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/entity/Career.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Career {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userEntity", nullable = false)
+    private User user;
+    @Column(nullable = false)
+    private String userId;
+    @Lob
+    @Column(nullable = false)
+    private String content;
+}
